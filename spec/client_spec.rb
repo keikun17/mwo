4922ea81520b30b1cd8c151c218dd95b829eaff7
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 01:23:47 +0800

    Rspec color + a few more specs, Ilkhan.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe MWO::Client do
   it "is initialized"
+
+  it "raises and error when the api cannot be accessed"
+
+  it "returns a json object"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'pry'
 require 'mwo'
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.failure_color = :magenta
+end

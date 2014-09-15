5613af9ed07858a2ef5ca9cd1992d7adda2fa577
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 13:26:15 +0800

    Move。

--- spec/mwo/client_spec.rb
+++ spec/mwo/client_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe MWO::Client do
-  it "is initialized"
+
+  it "is initialized with a url" do
+    url = ''
+  end
 
   it "raises and error when the api cannot be accessed"
 

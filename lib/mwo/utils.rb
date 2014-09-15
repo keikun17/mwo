0a2d5bf8ef87623b6012e85229dee611b8e73313
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 22:13:16 +0800

    to_symbol helper.

--- lib/mwo/utils.rb
+++ lib/mwo/utils.rb
@@ -4,4 +4,15 @@ module MWO::Utils
     body = JSON.parse(response.read)
   end
 
+  def to_symbol(string)
+    word = string.dup
+    word.gsub!(/::/, '/')
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+    word.tr!('-', '_')
+    word.downcase!
+    word.to_sym
+  end
+
 end
+

23674358fb067607e2cff7bf52ab1b6c310236cf
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 14:55:46 +0800

    URLs。

--- lib/mwo/client.rb
+++ lib/mwo/client.rb
@@ -1,2 +1,27 @@
 class MWO::Client
+  ITEMS_PATH     = "/api/items/list/full.json"
+  OMNIPARTS_PATH = "/api/omniparts/list/full.json"
+  MECH_IDS_PATH  = "/api/mechs/list/dict.json"
+
+  attr_accessor :host
+
+  def initialize(args = {})
+    @host = args[:host] || "http://static.mwomercs.com"
+  end
+
+  def items
+  end
+
+  def items_url
+    host + ITEMS_PATH
+  end
+
+  def omniparts_url
+    host + OMNIPARTS_PATH
+  end
+
+  def mech_ids_url
+    host + MECH_IDS_PATH
+  end
+
 end
--- spec/mwo/client_spec.rb
+++ spec/mwo/client_spec.rb
@@ -2,11 +2,27 @@ require 'spec_helper'
 
 describe MWO::Client do
 
-  it "is initialized with a url" do
-    url = ''
+  let(:hosto) { 'http://static.mwomercs.com' }
+  subject(:client) { described_class.new(hosto: hosto) }
+
+  describe "#items_url" do
+    it "returns the url for items api"  do
+      expect(client.items_url).to eq("http://static.mwomercs.com/api/items/list/full.json")
+    end
+  end
+
+  describe "#omniparts_url" do
+    it "returns the url for items api"  do
+      expect(client.omniparts_url).to eq("http://static.mwomercs.com/api/omniparts/list/full.json")
+    end
+  end
+
+  describe "#mech_ids" do
+    it "returns the url for items api"  do
+      expect(client.mech_ids_url).to eq("http://static.mwomercs.com/api/mechs/list/dict.json")
+    end
   end
 
   it "raises and error when the api cannot be accessed"
 
-  it "returns a json object"
 end

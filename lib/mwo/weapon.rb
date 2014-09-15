e86169c19a318c6b702c0b3aaf071d1142379108
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 22:13:39 +0800

    Weapon attributes.

--- lib/mwo/weapon.rb
+++ lib/mwo/weapon.rb
@@ -1,5 +1,13 @@
 class MWO::Weapon
+  attr_accessor :weapon_id, :name, :type, :num_firing, :damage, :heatpenalty, :heat, :factions
+
   extend MWO::Utils
+
+  def initialize args = {}
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
 
   def self.all(overrides: {})
     client = MWO::Client.new(overrides: {})

ad6dc33fbcd0c2d5138b7faa191c78c0d2d90b81
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 22:14:10 +0800

    Implement weapon fetching.

--- lib/mwo/weapon.rb
+++ lib/mwo/weapon.rb
@@ -11,6 +11,36 @@ class MWO::Weapon
 
   def self.all(overrides: {})
     client = MWO::Client.new(overrides: {})
-    fetch(client.items_url)
+    raw = fetch(client.items_url)
+
+    weapons = []
+
+    raw.each do |weapon_id, attrs|
+      if attrs["category"] == "weapon"
+
+
+        weapon = {weapon_id: weapon_id.to_i,
+                  name: attrs["name"],
+                  category: attrs["category"],
+                  stats: {}}
+
+        if attrs["factions"]
+          weapon[:factions] = {}
+          attrs["factions"].each do |k,v|
+            weapon[:factions][to_symbol(k)] = v
+          end
+        end
+
+        attrs["stats"].each do |k,v|
+          weapon[to_symbol(k)] = v
+        end
+
+        weapons << new(weapon)
+      end
+
+    end
+
+    return weapons
+
   end
 end

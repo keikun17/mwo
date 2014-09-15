5a2ce95bcdd82712f2c7d311aef59fb2b7e7b107
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 22:13:52 +0800

    Weapon Specs.

--- spec/mwo/utils_spec.rb
+++ spec/mwo/utils_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 class UtilUserClass
-  extend MWO::Utils
 end
 
 describe UtilUserClass do
+
+  before do
+    described_class.extend(MWO::Utils)
+  end
 
   describe ".fetch", vcr: {cassette_name: 'utils_fetch_url'} do
     let(:url) { "http://static.mwomercs.com/api/items/list/full.json"}

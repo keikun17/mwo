ca60a96ef0f709595ecbc27b193b3dc88cc51959
Buddy Magsipoc <keikun17@gmail.com>
2014-09-15 20:29:36 +0800

    VCR.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
 require 'pry'
 require 'mwo'
+require 'vcr'
 
 RSpec.configure do |config|
   config.color = true
   config.tty = true
   config.failure_color = :magenta
+end
+
+VCR.configure do |vcr|
+  vcr.cassette_library_dir = 'fixtures/cassettes'
+  vcr.default_cassette_options = {record: :once}
+  vcr.configure_rspec_metadata!
+  vcr.hook_into :webmock
 end

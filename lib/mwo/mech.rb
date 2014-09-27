046851409b35fb1d1ee39942c9d3a42fab59ae3f
Buddy Magsipoc <keikun17@gmail.com>
2014-09-27 09:53:50 +0800

    GTFO.

--- lib/mwo/mech.rb
+++ lib/mwo/mech.rb
@@ -1,12 +1,6 @@
 class MWO::Mech < OpenStruct
   extend MWO::Utils
 
-  # def initialize args = {}
-  #   args.each do |k,v|
-  #     instance_variable_set("@#{k}", v) unless v.nil?
-  #   end
-  # end
-
   def self.all
     client = MWO::Client.new(overrides: {})
     raw = fetch(client.mechs_url)
